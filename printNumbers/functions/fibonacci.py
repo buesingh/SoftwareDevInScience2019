24ef2dd0fc40f57565a77dcaadd1016b7dcd5900
Henrik Buesing <h.buesing@fz-juelich.de>
2019-11-19 14:59:23 +0100

    Fibonacci without recursion.

--- printNumbers/functions/fibonacci.py
+++ printNumbers/functions/fibonacci.py
@@ -23,10 +23,24 @@ def FibonacciRecursion(n):
     '''
     Helper function.
     '''
-    if n <= 1:
-        return n
-    else:
-        return (FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2))
+#    if n <= 1:
+#        return n
+#    else:
+#        return (FibonacciRecursion(n - 1) + FibonacciRecursion(n - 2))
+    a = 0
+    b = 1
+    if n < 0: 
+        print("Incorrect input") 
+    elif n == 0: 
+        return a 
+    elif n == 1: 
+        return b 
+    else: 
+        for i in range(2,n+1): 
+            c = a + b 
+            a = b 
+            b = c 
+        return b 
 
 def FibonacciSequence(n):
     '''
